a4e0540cedec924dba159b3d2eaff5b172fb5fe1
drewripa <ripa.andrew@gmail.com>
2017-12-09 17:54:15 +0200

    game mode start added (not tested on W OS, left with Fixed for RH OS)

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -38,3 +38,22 @@ def game(message):
     utils.set_user_game(message.chat.id, row[2])
     # Отсоединяемся от БД
     db_worker.close()
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def check_answer(message):
+    # Если функция возвращает None -> Человек не в игре
+    answer = utils.get_answer_for_user(message.chat.id)
+    # Как Вы помните, answer может быть либо текст, либо None
+    # Если None:
+    if not answer:
+        bot.send_message(message.chat.id, 'Чтобы начать игру, выберите команду /game')
+    else:
+        # Уберем клавиатуру с вариантами ответа.
+        keyboard_hider = types.ReplyKeyboardRemove()
+        # Если ответ правильный/неправильный
+        if message.text == answer:
+            bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
+        else:
+            bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз!', reply_markup=keyboard_hider)
+        # Удаляем юзера из хранилища (игра закончена)
+        utils.finish_user_game(message.chat.id)
